3e82834f795585babf5a4382ef076f13c02dbc82
[Blavien] <[sara.garax@gmail.com]>
2023-05-10 16:04:04 +0100

    add : Google TTS is working, Added ARI play example. Needs testing

---
+++ src/go.mod
@@ -0,0 +1,36 @@
+module Test
+
+go 1.20
+
+require (
+	cloud.google.com/go v0.110.0 // indirect
+	cloud.google.com/go/compute v1.19.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/longrunning v0.4.1 // indirect
+	cloud.google.com/go/texttospeech v1.6.1 // indirect
+	github.com/CyCoreSystems/ari/v6 v6.0.0-20230215145006-2a459024fa22 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/s2a-go v0.1.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.8.0 // indirect
+	github.com/inconshreveable/log15 v0.0.0-20201112154412-8562bdadbbac // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/rotisserie/eris v0.4.1 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/api v0.118.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/grpc v1.55.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,132 @@
+// Command quickstart generates an audio file with the content "Hello, World!".
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/CyCoreSystems/ari/v6/ext/play"
+	"github.com/inconshreveable/log15"
+
+	"github.com/CyCoreSystems/ari/v6"
+	"github.com/CyCoreSystems/ari/v6/client/native"
+	"io/ioutil"
+
+	texttospeech "cloud.google.com/go/texttospeech/apiv1"
+	"cloud.google.com/go/texttospeech/apiv1/texttospeechpb"
+)
+
+var log = log15.New()
+
+func main() {
+	_, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// connect
+	native.Logger = log
+
+	log.Info("Connecting to ARI")
+	/*cl, err := native.Connect(&native.Options{
+		Application:  "test",
+		Username:     "admin",
+		Password:     "admin",
+		URL:          "http://localhost:8088/ari",
+		WebsocketURL: "ws://localhost:8088/ari/events",
+	})
+	if err != nil {
+		log.Error("Failed to build ARI client", "error", err)
+		return
+	}
+
+	// setup app
+
+	log.Info("Listening for new calls")
+	sub := cl.Bus().Subscribe(nil, "StasisStart")
+
+	for {
+		select {
+		case e := <-sub.Events():
+			v := e.(*ari.StasisStart)
+			log.Info("Got stasis start", "channel", v.Channel.ID)
+			go app(ctx, cl.Channel().Get(v.Key(ari.ChannelKey, v.Channel.ID)))
+		case <-ctx.Done():
+			return
+		}
+	}*/
+}
+
+func text_to_speech(audioText string) {
+	// Instantiates a client.
+	ctx := context.Background()
+
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		log.Error(err.Error())
+	}
+	defer client.Close()
+
+	// Perform the text-to-speech request on the text input with the selected
+	// voice parameters and audio file type.
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		// Set the text input to be synthesized.
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: audioText},
+		},
+		// Build the voice request, select the language code ("en-US") and the SSML
+		// voice gender ("neutral").
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: "pt-PT",
+			SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
+		},
+		// Select the type of audio file you want returned.
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+		},
+	}
+
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
+		log.Error(err.Error())
+	}
+
+	// The resp's AudioContent is binary.
+	filename := "output.mp3"
+	err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+	if err != nil {
+		log.Error(err.Error())
+	}
+	fmt.Printf("Audio content written to file: %v\n", filename)
+}
+
+func app(ctx context.Context, h *ari.ChannelHandle) {
+	defer h.Hangup()
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	log.Info("Running app", "channel", h.ID())
+
+	end := h.Subscribe(ari.Events.StasisEnd)
+	defer end.Cancel()
+
+	// End the app when the channel goes away
+	go func() {
+		<-end.Events()
+		cancel()
+	}()
+
+	if err := h.Answer(); err != nil {
+		log.Error("failed to answer call", "error", err)
+		return
+	}
+
+	audioText := "Hallo"
+	text_to_speech(audioText)
+
+	if err := play.Play(ctx, h, play.URI("file://output.mp3")).Err(); err != nil {
+		log.Error("failed to play sound", "error", err)
+		return
+	}
+
+	log.Info("completed playback")
+	return
+}
